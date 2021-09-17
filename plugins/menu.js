85806d871c950f2c6e3e044ad30fc0bd12ef9033
unxzx <84166927+unx21@users.noreply.github.com>
2021-09-17 10:30:39 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -59,6 +59,10 @@ ${'```%npmdesc```'}
 let handler = async (m, { conn, usedPrefix: _p }) => {
   try {
     let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let wabot-aq02 = './src/photo/JdA.png'
+    let bp02 = 'https://github.com/bintangp02'
+    //let premium = global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+    let tnbot = (await conn.getFile(await conn.getProfilePicture(m.fromMe))).data.toString('base64')
     let { exp, limit, level, role } = global.db.data.users[m.sender]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let name = conn.getName(m.sender)
@@ -152,7 +156,24 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.reply(m.chat, text.trim(), m)
+    //conn.reply(m.chat, text.trim(), m)
+    await conn.sendFile(m.chat, wabot-aq02, 'JdA.png', text.trim(), { 
+      key: { 
+        remoteJid: 'status@broadcast', 
+        participant: '0@s.whatsapp.net', 
+        fromMe: false 
+      }, 
+      message: { 
+        "imageMessage": { 
+          "mimetype": "image/jpeg", 
+          "caption": `${conn.user.name} Verified Bot`, 
+          "jpegThumbnail": tnbot
+        } 
+      }
+    }, m, { 
+      //thumbnail: tnbot, 
+      contextInfo: { 
+        mentionedJid: [m.sender]} } )
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
