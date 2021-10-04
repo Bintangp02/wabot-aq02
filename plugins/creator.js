a7d9a693b5c097484847783971fe5ee6840f1c01
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2021-10-04 19:11:06 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6289647417373', 'Bintangp02', m)
+  // F this.sendContact(m.chat, '6285701399751', 'hdiiofficial', m)
+  this.sendContact(m.chat, '18773291551', 'hdiiofficial', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
