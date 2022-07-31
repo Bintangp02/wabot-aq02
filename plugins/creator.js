3c2f71721bed62aea902c616de033bdde56cd179
ZexG <109890319+ZexG@users.noreply.github.com>
2022-08-01 05:30:42 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6289647417373', 'Bintangp02', m)
+  this.sendContact(m.chat, '6281327366008', 'ZeX-Makerz! ', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
